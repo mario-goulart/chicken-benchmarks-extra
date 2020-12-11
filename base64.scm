84288c1d49a1c72d6089daed94bc2387d58dc9ee
Mario Domenech Goulart <mario@parenteses.org>
2020-12-12 00:02:55 +0100

    Add base64.scm

---
+++ base64.scm
@@ -0,0 +1,26 @@
+(import (chicken file)
+        (chicken port))
+(import base64)
+
+(define (encode str)
+  (call-with-input-string str
+    (lambda (port)
+      (base64-encode port))))
+
+(let ((files (cons "data/salmonella/salmonella.log"
+                   (append
+                    (list "data/chicken-core/runtime.c"
+                          "data/chicken-core/chicken-do.c"
+                          "data/chicken-core/dbg-stub.c")
+                    (glob "data/chicken-core/Makefile*")
+                    (glob "data/chicken-core/*.h")
+                    (glob "data/chicken-core/*.mdoc")
+                    (glob "data/chicken-core/*.scm")
+                    (glob "data/chicken-core/tests/*.scm")
+                    (glob "data/chicken-core/scripts/*")
+                    ))))
+  (time (map (lambda (file)
+               (call-with-input-file file
+                 (lambda (port)
+                   (encode (encode (encode (base64-encode port)))))))
+             files)))
