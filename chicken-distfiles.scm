9280eb164c8f03a665d861e5aaf51a0d8c630d09
Mario Domenech Goulart <mario@parenteses.org>
2021-01-06 13:58:31 +0100

    Prepend CHICKEN installation prefix to $PATH
    When calling make for chicken-distfiles and manual-labor, we have to
    prepend the installation prefix to $PATH, otherwise CHICKEN tools from
    CHICKEN installation not necessarily being benchmarked would be used.

--- chicken-distfiles.scm
+++ chicken-distfiles.scm
@@ -1,8 +1,12 @@
 (import (chicken base)
+        (chicken format)
         (chicken process))
 
 (include "lib/bench-utils.scm")
 
 (system* "make -C data/chicken-core spotless")
 
-(fake-time (system* "make -C data/chicken-core distfiles"))
+(fake-time
+ (system*
+  (sprintf "PATH=~a-:$PATH make -C data/chicken-core distfiles"
+           (prefix))))
--- manual-labor.scm
+++ manual-labor.scm
@@ -1,8 +1,12 @@
 (import (chicken base)
+        (chicken format)
         (chicken process))
 
 (include "lib/bench-utils.scm")
 
 (system* "rm -rf data/chicken-core/manual-html")
 
-(fake-time (system* "make -C data/chicken-core html"))
+(fake-time
+ (system*
+  (sprintf "PATH=~a:$PATH make -C data/chicken-core html"
+           (prefix))))
